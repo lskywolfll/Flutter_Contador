fb176d412d899f596015efb58e90945ab60c56f1
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-07-19 19:27:35 -0400

    se agregan icono de agregar mas y en proceso de crear un widget con estados para el contador de la pagina

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final estiloTextoContenido =
+      new TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
+  final estiloTextoNumero = new TextStyle(fontSize: 25);
+  final conteo = 10;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +19,28 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text('Contenido!'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Cantidad de clicks: ',
+              style: estiloTextoContenido,
+            ),
+            Text(
+              '$conteo',
+              style: estiloTextoNumero,
+            )
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('HolA!!!!');
+        },
+        child: Icon(Icons.add),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
